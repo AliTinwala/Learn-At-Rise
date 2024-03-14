bcf22b107dfbf51513b34f72df36f2785ff8b15c
AliTinwala <68299741+AliTinwala@users.noreply.github.com>
2024-03-14 11:56:20 +0530

    Update 15-03-24.sql

--- SQL/March/15-03-24/15-03-24.sql
+++ SQL/March/15-03-24/15-03-24.sql
@@ -1,3 +1,10 @@
 use FullStack;
 
-select * from emptbl2;
+create table emptbl
+(
+	EmpID int primary key identity(100,1),
+	EmpName varchar(20),
+	DeptID int not null,
+	TeamID int not null,
+	Process_training
+)
