7e142afcb4f4fe01e5d6ac98c83f827b255e57f0
AliTinwala <68299741+AliTinwala@users.noreply.github.com>
2024-03-15 15:46:57 +0530

    DotNet

--- DotNet/DotNet Projects/obj/Debug/net5.0/DotNet Projects.AssemblyInfo.cs
+++ DotNet/DotNet Projects/obj/Debug/net5.0/DotNet Projects.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNet Projects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d83d2526a1bfa8ddf2fa7f73c6e76aa42c49527d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb5092ffe178a237e7a3e49f5cdbdd2d57fe6c70")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNet Projects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNet Projects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
