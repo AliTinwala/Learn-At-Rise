b53e0c44a9b4ca097ec5761d286aa49b7eeed664
AliTinwala <68299741+AliTinwala@users.noreply.github.com>
2024-03-20 10:44:20 +0530

    Update 19-03-24 Task.sql

--- SQL/March/19-03-24 Task/19-03-24 Task.sql
+++ SQL/March/19-03-24 Task/19-03-24 Task.sql
@@ -35,27 +35,61 @@ end
 
 select dbo.FN_NoOfProductsByColour('Road Frames','Black')
 
--- 3. Select total unit price of product category. (Subquery)
-create or alter function FN_TotalUnitPriceOfProduct(@product varchar(30))
+-- 3. Select average unit price of product. (Subquery)
+create or alter function FN_AvgUnitPriceOfProduct(@product varchar(40))
 returns int
 as
 begin
-declare @total int
-select @total = sum(SalesLT.SalesOrderDetail.UnitPrice) from SalesLT.SalesOrderDetail
+declare @avg money
+select @avg = avg(SalesLT.SalesOrderDetail.UnitPrice) from SalesLT.SalesOrderDetail
+where SalesLT.SalesOrderDetail.ProductID in (select  ProductID from SalesLT.Product
+where SalesLT.Product.[Name] = @product)
+return @avg
+end
 
+select dbo.FN_AvgUnitPriceOfProduct('ML Road Frame-W - Yellow, 48')
+
+-- 4. Select total cost of product. (Subquery)
+create or alter function FN_TotalUnitPriceOfProduct(@product varchar(40))
+returns int
+as
+begin
+declare @total money
+select @total = sum(SalesLT.SalesOrderDetail.LineTotal) from SalesLT.SalesOrderDetail
+where SalesLT.SalesOrderDetail.ProductID in (select  ProductID from SalesLT.Product
+where SalesLT.Product.[Name] = @product)
 return @total
 end
 
-select dbo.FN_TotalUnitPriceOfProduct('Road Frames','Black')
+select dbo.FN_TotalUnitPriceOfProduct('ML Road Frame-W - Yellow, 48')
 
+-- 5. Find highest Subtotal value of product using subquery
+create or alter function HighestBillForProduct(@CustomerID  int)
+returns Money
+as
+begin
+    declare @subtotal money;
+    select @subtotal = max(SalesLT.SalesOrderHeader.SubTotal)
+    from SalesLT.SalesOrderHeader 
+	where SalesLT.SalesOrderHeader.CustomerID in (select SalesLT.Customer.CustomerID from SalesLT.Customer 
+	where CustomerID = @CustomerID)
+	return @subtotal;
+end;
 
--- 4.  
--- 5. 
--- 6. 
--- 7. 
+select dbo.HighestBillForProduct(29847);
+
+-- 6.
+-- 7.  
 
 -- Inline Functions
--- 1. 
+-- 1.  GetProductDetailsbyOrder
+create or alter function GetProductDetailsByOrder(@ProductID int)
+Returns table 
+As
+return (Select SOD.ProductID , SP.Name As productName , SOD.UnitPrice As UnitPrice , SOD.OrderQty As Orderqty   From SalesLT.SalesOrderDetail SOD 
+join SalesLT.Product SP on SP.ProductID = SOD.ProductID Where SP.ProductID = @ProductID)
+
+select * from dbo.GetProductDetailsByOrder(836);
 -- 2. 
 -- 3. 
 -- 4.  
