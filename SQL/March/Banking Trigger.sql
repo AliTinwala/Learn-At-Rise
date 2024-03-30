4e4d9c359ff83f00698325f971f9fae9e7bc22a9
AliTinwala <68299741+AliTinwala@users.noreply.github.com>
2024-03-30 12:22:58 +0530

    Update Banking Trigger.sql

--- SQL/March/Banking Trigger.sql
+++ SQL/March/Banking Trigger.sql
@@ -2,16 +2,18 @@ CREATE DATABASE Trg_BankingSystem
 
 use Trg_BankingSystem
 
+drop table txntbl
+
 create table txntbl
 (
 	id int identity(1,1) constraint PK_id primary key,
 	acc_id int not null,
-	dateoftxn datetime not null default(getdate()),
+	dateoftxn date not null default(getdate()),
 	typeoftxn char(2) not null check (typeoftxn in ('Cheque','CW','CD')),
 	txn_amt money not null
 )
 
-create table acc_mastertbl
+create table or alter acc_mastertbl
 (
 	acc_id int identity(1,1) not null,
 	acc_holder_name varchar(20) not null,
@@ -19,16 +21,14 @@ create table acc_mastertbl
 	acc_status varchar(20) not null constraint chk_status check(acc_status in ('Active','Inactive'))
 )
 
-create table daily_txntbl
-(
-	acc_id int not null,
-	dateoftxn datetime default(getdate()),
-	countofCW int default 0
-)
-
 insert into acc_mastertbl values('Ali',0,'Active')
+GO
 
 select * from acc_mastertbl
+go
+
+drop trigger trg_balanceUpdates
+go
 
 create or alter trigger trg_balanceUpdates
 on txntbl
@@ -41,14 +41,15 @@ begin
 	@typeoftxn varchar(20),
 	@dateoftxn datetime,
 	@amount money,
-	@netbalace money,
+	@netbalance money,
 	@countdate int,
 	@totalamt money
 
-	set @countdate = 1
+	set @countdate = (select acc_id,dateoftxn,count(*) from txntbl where typeoftxn = 'CW' and acc_id = 1 GROUP BY dateoftxn,acc_id)
+
 	set @totalamt = 0
 
-	select @id = acc_id , @typeoftxn = typeoftxn, @amount = txn_amt,@dateoftxn = dateoftxn   from inserted
+	select @id = acc_id , @typeoftxn = typeoftxn, @amount = txn_amt,@dateoftxn = dateoftxn from inserted
 
 	select @status = acc_status from acc_mastertbl where acc_id = @id
 
@@ -67,19 +68,44 @@ begin
 		end
 		else if (@typeoftxn = 'CW')
 		begin
-			print ''	
+			set @totalamt = @totalamt + @amount
+			print @totalamt
+			if(@countdate <= 3 and @totalamt <= 40000)
+			BEGIN
+				print @countdate
+				update acc_mastertbl
+				set netbalance = netbalance - @amount
+				where acc_id = @id
+			END
+			else if(@totalamt between 40001 and 100000 and @countdate = 1)
+			BEGIN
+				print @countdate
+				update acc_mastertbl
+				set netbalance = netbalance - (@amount + (@amount * 0.1))
+				where acc_id = @id
+			END
+			ELSE
+			BEGIN
+				print @countdate
+				print 'You cannot withdraw total more than 40000 more than 3 times a day'
+				ROLLBACK
+			END
 		end
+	end
 end
 
 select * from acc_mastertbl
 select * from txntbl
-select * from daily_txntbl
 
 insert into acc_mastertbl values('Ali',0,'Active')
+insert into acc_mastertbl values('Het',0,'Active')
 
 insert into txntbl (acc_id,typeoftxn,txn_amt) values(1,'CD',100000)
+insert into txntbl (acc_id,typeoftxn,txn_amt) values(2,'CD',100000)
 
 insert into txntbl (acc_id,typeoftxn,txn_amt)values(1,'CW',41000)
+insert into txntbl (acc_id,typeoftxn,txn_amt)values(2,'CW',41000)
+
 
 truncate table txntbl
 truncate table acc_mastertbl
