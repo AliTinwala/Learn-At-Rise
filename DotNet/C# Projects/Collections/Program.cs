62c9c96d1b7f5d9d970a022f404de685e1a01465
AliTinwala <68299741+AliTinwala@users.noreply.github.com>
2024-04-22 17:37:28 +0530

    22-04-24

---
+++ DotNet/C# Projects/Collections/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Collections
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> l1 = new List<int> { };
+            l1.Add(1);
+            l1.Add(2);
+            l1.Add(3);
+
+            foreach(int i in l1) { Console.WriteLine(i); }
+            Console.ReadLine();
+        }
+    }
+}
--- DotNet/C# Projects/Protected Internal/obj/Debug/net5.0/Protected Internal.AssemblyInfo.cs
+++ DotNet/C# Projects/Protected Internal/obj/Debug/net5.0/Protected Internal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Protected Internal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd80b94f53f0046a834498ab0a358895f6c38cdf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66a7f0a5fd5b6f759d4e45b75a33cd03ce05bbf3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Protected Internal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Protected Internal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
