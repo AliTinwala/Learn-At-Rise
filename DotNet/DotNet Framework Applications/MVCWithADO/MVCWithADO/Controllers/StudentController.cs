860df18672684026b6842ae47ca2d8a5c8f18e06
AliTinwala <68299741+AliTinwala@users.noreply.github.com>
2024-04-28 18:39:39 +0530

    Dotnet

---
+++ DotNet/DotNet Framework Applications/MVCWithADO/MVCWithADO/Controllers/StudentController.cs
@@ -0,0 +1,99 @@
+using MVCWithADO.Models;
+using MVCWithADO.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWithADO.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        StudentRepo objstudentRepo = new StudentRepo();
+
+        public ActionResult Index()
+        {
+            return View(objstudentRepo.GetStudents());
+        }
+
+        // get: student/details/5
+        public ActionResult Details(int id)
+        {
+            objstudentRepo.GetStudents().Find(Student => Student.S_Id == id);
+            return RedirectToAction("Index");
+            
+        }
+
+        // GET: Student/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        [HttpPost]
+        public ActionResult Create(StudentModel objstudent)
+        {
+            try
+            {
+                if(objstudentRepo.AddStudent(objstudent)) 
+                {
+                    ViewBag.Message = "Student Details Added Sucessfully";
+
+                }
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(objstudentRepo.GetStudents().Find(Student => Student.S_Id == id));
+        }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, StudentModel objstudent)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                objstudentRepo.Updatetudent(objstudent);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    
+        // GET: Student/Delete/5
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                if (objstudentRepo.DeleteStudent(id))
+                {
+                    ViewBag.AlertMessage = "Student Deleted SucessFully..";
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ DotNet/DotNet Framework Applications/MVCWithADO/MVCWithADO/Models/StudentModel.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace MVCWithADO.Models
+{
+    public class StudentModel
+    {
+        [Key]
+        [Display(Name ="ID")]
+        public int S_Id { get; set; }
+
+        [Display(Name ="Name")]
+        [Required(ErrorMessage ="Student name is required..")]
+        public string S_Name { get; set; }
+
+        [Display(Name = "AGE")]
+        [Required(ErrorMessage = "Student AGE is required..")]
+        public int S_Age { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Student Email is required..")]
+        public string S_Email { get; set; }
+    }
+}
