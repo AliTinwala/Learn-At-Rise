88bd31685ae1dce57eff6c51f7c87977dc13d3d9
AliTinwala <68299741+AliTinwala@users.noreply.github.com>
2024-06-03 13:57:44 +0530

    Mealbooking

--- DotNet/C# Projects/HasARelationship/obj/Debug/net5.0/HasARelationship.AssemblyInfo.cs
+++ DotNet/C# Projects/HasARelationship/obj/Debug/net5.0/HasARelationship.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HasARelationship")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66a7f0a5fd5b6f759d4e45b75a33cd03ce05bbf3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e4c1e1ca9ed3a728847514e9695bacaddecc87d")]
 [assembly: System.Reflection.AssemblyProductAttribute("HasARelationship")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HasARelationship")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
